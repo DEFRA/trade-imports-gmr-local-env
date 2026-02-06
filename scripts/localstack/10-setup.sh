224ea6bb2f35a45d73436ddb6b295f7c08fdaa1b
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-02-06 14:47:56 +0000

    BAU: WIP

--- scripts/localstack/10-setup.sh
+++ scripts/localstack/10-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# test-reports
+aws --endpoint-url $LOCALSTACK_URL s3api create-bucket --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION --bucket reports
+
 # trade-imports-gmr-finder
+aws --endpoint-url $LOCALSTACK_URL s3api create-bucket --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION --bucket trade-imports-gmr-finder-search-results
 aws --endpoint $LOCALSTACK_URL sns create-topic --region $AWS_REGION --name trade_imports_matched_gmrs
 aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_processor
 aws --endpoint $LOCALSTACK_URL sns subscribe --region $AWS_REGION --topic-arn arn:aws:sns:$AWS_REGION:000000000000:trade_imports_matched_gmrs --protocol sqs --notification-endpoint arn:aws:sqs:$AWS_REGION:000000000000:trade_imports_matched_gmrs_processor --attributes '{"RawMessageDelivery": "true"}'
@@ -8,6 +12,6 @@ aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-nam
 
 # trade-imports-gmr-processor
 aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-name trade_imports_data_upserted_gmr_processor_gto
-aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_processor_gto
 aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_gmr_processor_eta
+aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_gmr_processor_gto
 aws --endpoint $LOCALSTACK_URL sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_gmr_processor_match
