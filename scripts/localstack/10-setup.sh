cc59ca078c279042a7979ccb01d3f0ad7c6a1295
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-02-11 14:02:53 +0000

    PHAIN-247: Update services and configuration (#3)

---
+++ scripts/localstack/10-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# test-reports
+aws --endpoint-url $LOCALSTACK_ENDPOINT s3api create-bucket --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION --bucket reports
+
+# trade-imports-gmr-finder
+aws --endpoint-url $LOCALSTACK_ENDPOINT s3api create-bucket --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION --bucket trade-imports-gmr-finder-search-results
+aws --endpoint $LOCALSTACK_ENDPOINT sns create-topic --region $AWS_REGION --name trade_imports_matched_gmrs
+aws --endpoint $LOCALSTACK_ENDPOINT sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_processor
+aws --endpoint $LOCALSTACK_ENDPOINT sns subscribe --region $AWS_REGION --topic-arn arn:aws:sns:$AWS_REGION:000000000000:trade_imports_matched_gmrs --protocol sqs --notification-endpoint arn:aws:sqs:$AWS_REGION:000000000000:trade_imports_matched_gmrs_processor --attributes '{"RawMessageDelivery": "true"}'
+aws --endpoint $LOCALSTACK_ENDPOINT sqs create-queue --region $AWS_REGION --queue-name trade_imports_data_upserted_gmr_finder
+
+# trade-imports-gmr-processor
+aws --endpoint $LOCALSTACK_ENDPOINT sqs create-queue --region $AWS_REGION --queue-name trade_imports_data_upserted_gmr_processor_gto
+aws --endpoint $LOCALSTACK_ENDPOINT sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_gmr_processor_eta
+aws --endpoint $LOCALSTACK_ENDPOINT sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_gmr_processor_gto
+aws --endpoint $LOCALSTACK_ENDPOINT sqs create-queue --region $AWS_REGION --queue-name trade_imports_matched_gmrs_gmr_processor_match
+aws --endpoint $LOCALSTACK_ENDPOINT sns subscribe --region $AWS_REGION --topic-arn arn:aws:sns:$AWS_REGION:000000000000:trade_imports_matched_gmrs --protocol sqs --notification-endpoint arn:aws:sqs:$AWS_REGION:000000000000:trade_imports_matched_gmrs_gmr_processor_eta --attributes '{"RawMessageDelivery": "true"}'
+aws --endpoint $LOCALSTACK_ENDPOINT sns subscribe --region $AWS_REGION --topic-arn arn:aws:sns:$AWS_REGION:000000000000:trade_imports_matched_gmrs --protocol sqs --notification-endpoint arn:aws:sqs:$AWS_REGION:000000000000:trade_imports_matched_gmrs_gmr_processor_gto --attributes '{"RawMessageDelivery": "true"}'
+aws --endpoint $LOCALSTACK_ENDPOINT sns subscribe --region $AWS_REGION --topic-arn arn:aws:sns:$AWS_REGION:000000000000:trade_imports_matched_gmrs --protocol sqs --notification-endpoint arn:aws:sqs:$AWS_REGION:000000000000:trade_imports_matched_gmrs_gmr_processor_match --attributes '{"RawMessageDelivery": "true"}'
